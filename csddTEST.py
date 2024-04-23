477fcd34e67854f80be43a0134ac219f629ae643
Aleksejs Šepeļevs <118129406+AleksejsGIT@users.noreply.github.com>
2024-04-23 21:28:18 +0300

    Update csddTEST.py

--- csddTEST.py
+++ csddTEST.py
@@ -15,7 +15,7 @@ service = Service(executable_path=chrome_driver_path)
 driver = webdriver.Chrome(service=service, options=chrome_options)
 wait = WebDriverWait(driver, 600) # Gaidīšanas laiks, lai lapai ielādētos 10min
 
-def human_like_delay(minimum=2, maximum=5):
+def human_like_delay(minimum=3, maximum=5):
     time.sleep(random.uniform(minimum, maximum))
 
 link_text = "Darbības ar transportlīdzekļiem"
@@ -47,7 +47,7 @@ button_choose = "chooseNrSeries"
 find_button = wait.until(EC.element_to_be_clickable((By.ID, button_choose)))
 find_button.click()
 
-form_id = "uniforma_NZ106"  # ievadīt konkrētu numurzīmi, kuru pasūtīt
+form_id = "uniforma_WJ8"  # ievadīt konkrētu numurzīmi, kuru pasūtīt
 form = driver.find_element(By.ID, form_id)
 submit_button = form.find_element(By.XPATH, ".//input[@type='submit']")
 submit_button.click()
@@ -57,3 +57,34 @@ dropdown.select_by_value("L") # Rīga, Bauskas iela
 
 checkbox = driver.find_element(By.ID, "chkLaws")
 checkbox.click()
+
+
+# Tad te vajag ievadīt captcha, ar lieliem/maziem un poga turpināt.
+# Tālāk tikai viena poga Apstiprināt ar id ""payIzvNum"
+# Next divi checkmarki, saglabāt kartes datus un piekrīt maksājumu noteikumiem. (useOneClick)(chkLaws))
+
+
+text_name = "#############" 
+input_field_id1 = "cardname"
+input_field1 = wait.until(EC.element_to_be_clickable((By.ID, input_field_id1)))
+input_field1.send_keys(text_name)
+
+text_nr = "####################" 
+input_field_id2 = "cardnr"
+input_field2 = wait.until(EC.element_to_be_clickable((By.ID, input_field_id2)))
+input_field2.send_keys(text_nr)
+
+text_month = "##" 
+input_field_id3 = "expmonth"
+input_field3 = wait.until(EC.element_to_be_clickable((By.ID, input_field_id3)))
+input_field3.send_keys(text_month)
+
+text_year = "##" 
+input_field_id4 = "expyear"
+input_field4 = wait.until(EC.element_to_be_clickable((By.ID, input_field_id4)))
+input_field4.send_keys(text_year)
+
+text_cvc= "###" 
+input_field_id5 = "cvc2"
+input_field5 = wait.until(EC.element_to_be_clickable((By.ID, input_field_id5)))
+input_field5.send_keys(text_cvc)
